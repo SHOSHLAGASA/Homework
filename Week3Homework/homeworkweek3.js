44ed1a2fc0fc3d535633351b216c7f9e9735b51e
shoshana lagasa <shoshanalagasa850@gmail.com>
2021-12-07 22:25:29 -0600

    created folder week 3 homework

---
+++ Week3Homework/homeworkweek3.js
@@ -0,0 +1,29 @@
+//Creating an array of pizzaToppings with at least four different toppings
+const pizzaToppings = ["Onions,Tomatoes,Mushrooms,Corn"];
+//  create a listToppings function that stores the functionality, and call the function each time we list toppings in a function.
+function listToppings(pizzaToppings) {
+return  "Our toppings are ${pizzaToppings}"
+}
+function listToppings(callback) {
+    const pizzaToppings = listToppings;
+    alert(callback(listToppings));
+}
+//Create a greetCustomer function that prints a message that welcomes a guest, then informs them of the available toppings by looping
+function greetCustomer(listTopping) {
+    return "Welcome to Dominoes , ${listTopping} for (let pizzaTopping of pizzaToppings)";
+    console.log(pizzaTopping);
+}
+//Create a getPizzaOrder function that has the parameters
+function getPizzaOrder("Small, Medium, Large, thick, crust,[Onions,Tomatoes,Mushrooms,Corn]") {
+    console.log("Large, thick, crust, [Onions,Tomatoes,Mushrooms,Corn]")
+return One + large + thick +  pizza  + with + Onions + Tomatoes + Mushrooms + Corn  + coming up
+}  
+//Create a preparePizza function that has an array as its parameter with Object in key value
+obj["Cooking pizza"] = {
+    size: "large",
+    crus: "thick" ,
+    toppings: "Onions,Tomatoes,Mushrooms,Corn",
+  };
+  //Create a servePizza function that has a parameter of a pizza Object
+  function servePizza(large, thick, [Onions,Tomatoes,Mushrooms,Corn])
+  return "Order up! Here's your large thick crust pizza with Onions,Tomatoes,Mushrooms,Corn Enjoy!"
